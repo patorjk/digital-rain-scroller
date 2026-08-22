d5bcfbfd43d65de26b7a09b045e198ddd99c323a
Patrick Gillespie <patorjk@patricks-mini.lan>
2026-08-22 15:40:18 -0400

    initial work

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { DigitalRain } from '@/components/digital-rain/DigitalRain.tsx';
+
+function App() {
+  return (
+    <>
+      <DigitalRain rows={250} cols={50} />
+    </>
+  );
+}
+
+export default App;
