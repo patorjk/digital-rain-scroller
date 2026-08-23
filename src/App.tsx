3b40bb092fa730a9d9594ca4dd6670e0d37c729b
Patrick Gillespie <patorjk@Patricks-Mac-mini.local>
2026-08-23 02:50:40 -0400

    make cols adjust to resize

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import './App.css';
 import { DigitalRain } from '@/components/digital-rain/DigitalRain.tsx';
 import { CELL_WIDTH } from '@/components/digital-rain/dr-utils.ts';
+import { useRef } from 'react';
+import { useResizeDetector } from 'react-resize-detector';
 
 function App() {
-  const cols = Math.max(Math.floor(window.innerWidth / CELL_WIDTH), 1);
+  const htmlRef = useRef(document.documentElement);
+
+  const { width } = useResizeDetector({
+    targetRef: htmlRef,
+    refreshMode: 'debounce', // optional: throttle noisy resize events
+    refreshRate: 100,
+  });
+
+  if (!width) return null;
+  const cols = Math.max(Math.floor(width / CELL_WIDTH), 1);
 
   return (
     <>
-      <DigitalRain rows={1000} cols={cols} />
+      <DigitalRain rows={1000} cols={cols} key={cols} />
     </>
   );
 }
